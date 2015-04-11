1d86cef8395911c2f56f0d0657d5203fdd30f904
Johan <johan@Firefly.local>
2015-04-11 23:34:22 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name = "plugwise_graphite",
+    version = "0.0.1",
+    author = "Johan Bloemberg",
+    author_email = "mail@ijohan.nl",
+    description = ("Read metrics from plugwise circles and push to graphite."),
+    license = "BSD",
+    packages=['plugwise_graphite'],
+    install_requires=[
+        "plugwise",
+        "anyconfig",
+        "pyserial",
+        "crcmod",
+    ],
+    dependency_links=[
+        "hg+https://bitbucket.org/hadara/python-plugwise#egg=plugwise"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
